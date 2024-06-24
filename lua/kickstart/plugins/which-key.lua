4c78c25d56532f01180a0340e730f1c2e239c899
Ajeet Gill <ajeetshergill@gmail.com>
2024-06-24 01:32:18 -0300

    Update which-key.lua

--- lua/kickstart/plugins/which-key.lua
+++ lua/kickstart/plugins/which-key.lua
@@ -18,22 +18,36 @@ return {
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
+      local wk = require 'which-key'
+      wk.setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+
+      wk.register {
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
+        --   ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+          ['<leader>d'] = { name = '[D]elete' },
+        --   ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        -- ['<leader>.'] = { name = '[O]pen recent files', _ = 'which_key_ignore' },
+        --   ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
+        --   ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        --   ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
       }
       -- visual mode
-      require('which-key').register({
+      wk.register({
         ['<leader>h'] = { 'Git [H]unk' },
       }, { mode = 'v' })
+
+      -- wk.register {
+      --   ['<leader>'] = {
+      --     n = {
+      --       name = '+file',
+      --       f = { '<cmd>Telescope find_files<cr>', 'Find File' },
+      --       r = { '<cmd>Telescope oldfiles<cr>', 'Open Recent File' },
+      --       n = { '<cmd>enew<cr>', 'New File' },
+      --     },
+      --   },
+      -- }
     end,
   },
 }
